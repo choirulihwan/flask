f3416bab095457f2d41a63c848de54d4de3bfd79
choirulihwan <aristhu_oracle@yahoo.com>
2020-04-28 22:14:01 +0700

    add connectivity to postgresql using psycopg2

--- fundamental/web_app/app.py
+++ fundamental/web_app/app.py
@@ -14,6 +14,19 @@ def create_app():
     def about():
         return render_template('about.html', TITLE='Flask bootstrap')
 
+    @app.route('/testdb')
+    def testdb():
+        from psycopg2 import connect
+
+        conn = connect('dbname=flask_bootstrap user=choirul password=rahasia host=postgres')
+
+        cur = conn.cursor()
+        cur.execute('select * from page')
+        id, title = cur.fetchone()
+
+        conn.close()
+        return 'Output table page {} - {}'.format(id, title)
+
     return app
 
 # create_app().run('127.0.0.1', debug=True)
