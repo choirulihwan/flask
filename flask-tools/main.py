0b47e7380fabb17f4d0adcc3d4d05c6cea0c121a
choirul <aristhu_oracle@yahoo.com>
2023-09-19 11:26:54 +0700

    add pdf to excel

--- flask-tools/main.py
+++ flask-tools/main.py
@@ -6,13 +6,15 @@ from flask import render_template
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = 'files'
+UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'output'
 ALLOWED_EXTENSIONS = {'pdf'}
 ALLOWED_IMAGE_EXTENSIONS = {'jpg', 'jpeg', 'webp', 'png'}
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 
 @app.route("/")
@@ -22,7 +24,7 @@ def index():
 
 @app.route('/download/<name>', methods=['GET', 'POST'])
 def download_file(name):
-    path = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'], name)
+    path = os.path.join(current_app.root_path, app.config['OUTPUT_FOLDER'], name)
     return send_file(path, as_attachment=True)
 
 
@@ -32,6 +34,39 @@ def allowed_file(filename):
 
 def allowed_image(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
+
+
+# pdf to excel
+@app.route("/toexcel", methods=['GET', 'POST'])
+def toexcel():
+    if request.method == 'POST':
+        import subprocess
+
+        if 'filepdf' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['filepdf']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            filename_only = Path(file_path).stem
+            file.save(file_path)
+
+            # convert to excel
+            file_output = os.path.join(app.config['OUTPUT_FOLDER'], filename_only + ".csv")
+            import tabula
+            df = tabula.read_pdf(input_path=file_path, pages="all")
+            tabula.convert_into(input_path=file_path, output_path=file_output, output_format="csv", pages="all", stream=True)
+
+            return redirect(url_for('download_file', name=filename_only + ".csv"))
+    else:
+        return render_template('pdf_to_excel.html')
 
 
 # image module
@@ -51,22 +86,22 @@ def removebg():
             flash('No selected file')
             return redirect(request.url)
 
-        output_path = ''
         if file and allowed_image(file.filename):
             filename = secure_filename(file.filename)
-            file_path = os.path.join('static', filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
 
             input_path = file_path
             filename_only = Path(file_path).stem
-            output_path = 'static/' + filename_only + '_out.png'
+            output_path = os.path.join(app.config['OUTPUT_FOLDER'], filename_only + ".png")
 
-            input = Image.open(input_path)
-            output = remove(input)
+            input2 = Image.open(input_path)
+            output = remove(input2)
             output.save(output_path)
 
             flash('Process finished')
-        return render_template('remove_background.html', data=output_path)
+        # return render_template('remove_background.html', data=output_path)
+        return redirect(url_for('download_file', name=filename_only + ".png"))
     else:
         return render_template('remove_background.html')
 
@@ -98,16 +133,16 @@ def youtubedownloader():
 @app.route("/barcode", methods=['GET', 'POST'])
 def barcode():
     if request.method == 'POST':
-        import barcode
         from barcode import Code128
 
         data = request.form['barcode']
 
         code = Code128(data)
-        code.save("static/barcode")
+        code.save("output/barcode" + data)
         flash('Barcode generated')
 
-        return render_template('barcode.html', data=data)
+        # return render_template('barcode.html', data=data)
+        return redirect(url_for('download_file', name="barcode" + data + ".svg"))
     else:
         return render_template('barcode.html')
 
